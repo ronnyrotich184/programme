2ed72daee5fd77c1a5f5ba4ef4fd29e40d2622cf
RONNY KIMUTAI <ronnyrotich53@gmail.com>
2025-09-30 03:31:24 -0700

    Add files via upload

---
+++ volume.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+        float v,r,h;
+         printf("enter the value of r");
+         printf("enter the value of h");
+         scanf("%f,$h");
+         v=M_PI*(r,2)*h;
+          printf("volume=%f",v);
+
+	
+	return 0;
+}
