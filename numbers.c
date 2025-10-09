e757fcd927e4a3446ed92fabcf265478d59ce1b0
RONNY KIMUTAI <ronnyrotich53@gmail.com>
2025-10-09 04:09:34 -0700

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,13 @@
+/*
+REG NO:PA106/G/28713/25
+NAME:RONNY KIMUTAI
+DESCRIPTION:PROGRAM TO DISPLAY NUMBERS DECREASING
+*/
+#include <stdio.h>
+ 
+int main() {
+    for (int i =100; i >=50; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
